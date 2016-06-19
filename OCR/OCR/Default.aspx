ed8a7c04ae433fe320ca5538a7b370441a9cd71a
shivam01990 <shivam01990@gmail.com>
2016-06-19 08:21:29 +0530

    Module changes

---
+++ OCR/OCR/Default.aspx
@@ -0,0 +1,237 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" EnableEventValidation="true" Inherits="OCRExtractTable.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>OCR App</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <style>
+        body {
+            padding-top: 70px;
+            /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
+        }
+    </style>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+
+    <link href="content/bootstrap-fileinput/css/fileinput.css" rel="stylesheet" />
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+
+    <script src="https://code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
+    <!-- plus a jQuery UI theme, here I use "flick" -->
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.10.4/themes/flick/jquery-ui.css">
+    <link href="css/jquery-ui-slider-pips.css" rel="stylesheet" />
+    <script src="js/jquery-ui-slider-pips.js"></script>
+
+    <script src="js/jquery.browser.js"></script>
+    <script type="text/javascript" src="js/fileinput.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script src="js/fileinput_locale_LANG.js"></script>
+    <script src="js/jquery.Jcrop.js"></script>
+    <link href="css/jquery.Jcrop.css" type="text/css" rel="stylesheet" />
+
+
+    <script type="text/javascript">
+        var rootpath = '<%=Page.ResolveUrl("~")%>';
+        $(document).ready(function () {
+
+            $("#file_BrandImage").fileinput({
+                uploadUrl: rootpath + 'FileUploadHandler.ashx',
+                uploadAsync: true,
+                allowedFileExtensions: ['jpg', 'png', 'gif']
+            });
+
+            $("#file_BrandImage").on('fileuploaded', function (event, data, previewId, index) {
+                //var form = data.form, files = data.files, extra = data.extra,
+                //    response = data.response, reader = data.reader; 
+                $.each(data.files, function (k, obj) {
+                    $("[id$=hdnUploadedImage]").val(obj["name"]);
+                    $("[id$=imgUpload]").attr('src', rootpath + "uploads/" + $("[id$=hdnUploadedImage]").val())
+                    $("[id$=ocr-sec]").show();
+
+                    $('#<%=imgUpload.ClientID%>').Jcrop({
+                        onSelect: SelectCropArea
+                    });
+
+                });
+            })
+
+            $('#file_BrandImage').on('fileclear', function (event) {
+                $("[id$=hdnUploadedImage]").val('');
+                $("[id$=ocr-sec]").hide();
+            });
+
+        });
+
+        function DrawSlider(totalColumns) {
+
+            if ($('#alternating-slider').length > 0) {
+                $('#alternating-slider').remove();
+
+            }
+            $('#alternating-slider-parent').append("<div id=\"alternating-slider\"></div>");
+
+            var distributedwidth = 0;
+            if (totalColumns > 0) {
+                distributedwidth = (100 / totalColumns).toFixedDown(2);
+            }
+
+            var arr = [];
+
+            for (var i = 0; i < totalColumns; i++) {
+                arr.push((i + 1) * distributedwidth);
+
+            }
+            arr.push()
+
+            $("#alternating-slider").slider({
+                max: 100,
+                values: arr
+            }).slider("pips", {
+                step: 5,
+                rest: "label",
+                labels: { first: "Min", last: "Max" }
+            }).slider("float");
+        }
+
+       
+
+        var currentcolumn = 1;
+
+        function AddColumn() {
+            currentcolumn++;
+            DrawSlider(currentcolumn);
+        }
+        function RemoveColumn() {
+            if (currentcolumn > 1) {
+                currentcolumn--;
+                DrawSlider(currentcolumn);
+            }
+
+        }
+
+
+        function SelectCropArea(c) {
+            $('#<%=X.ClientID%>').val(parseInt(c.x));
+            $('#<%=Y.ClientID%>').val(parseInt(c.y));
+            $('#<%=W.ClientID%>').val(parseInt(c.w));
+            $('#<%=H.ClientID%>').val(parseInt(c.h));
+        }
+
+
+
+
+        Number.prototype.toFixedDown = function (digits) {
+            var re = new RegExp("(\\d+\\.\\d{" + digits + "})(\\d)"),
+                m = this.toString().match(re);
+            return m ? parseFloat(m[1]) : this.valueOf();
+        };
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">OCR APP</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+
+        <!-- Page Content -->
+        <div class="container">
+            <div class="row">
+                <h2>
+                    <label>Upload File</label></h2>
+                <div class="col-sm-12 text-center">
+                    <input id="file_BrandImage" type="file" data-min-file-count="1">
+                </div>
+            </div>
+            <!-- /.row -->
+            <div id="ocr-sec" style="display: none;" class="col-sm-12">
+                <div class="row">
+                    <h2>
+                        <label>Upload File Preview</label></h2>
+                    <div class="form-horizontal">
+                        <div class="form-group">
+                            <asp:Image ID="imgUpload" runat="server" />
+                        </div>
+                        <div class="form-group">
+                            <asp:Button ID="btnCroppedImage" runat="server" Text="Read Cropped Image" CssClass="btn btn-primary" OnClick="btnCroppedImage_Click" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="divOcrProcess" visible="false" runat="server" style="margin-top: 50px;">
+                <div class="form-horizontal">
+                    <div class="form-group col-sm-7">
+                        <div class="col-sm-12">
+                            <span class="glyphicon glyphicon-plus-sign" aria-hidden="true" title="Add Column" onclick="AddColumn()"></span>
+                            <span class="glyphicon glyphicon-minus-sign" aria-hidden="true" title="Remove Column" onclick="RemoveColumn()"></span>
+
+                        </div>
+                        <div class="col-sm-12" id="alternating-slider-parent" style="margin-bottom:20px;">
+                            <div id="alternating-slider"></div>
+                        </div>
+                        <div class="col-sm-12">
+                            <asp:Image ID="imgCropped" Width="100%" runat="server" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">Total Columns</label>
+                        <div class="col-sm-10">
+                            <asp:TextBox ID="txtColumns" runat="server" class="form-control" placeholder="Columns"></asp:TextBox>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <asp:Button ID="btnOCRReader" runat="server" Text="Read Image Data" CssClass="btn btn-primary" OnClick="btnOCRReader_Click" OnClientClick="return readAlltextbox();" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.container -->
+        <asp:HiddenField ID="hdnUploadedImage" runat="server" />
+        <asp:HiddenField ID="hdnCroppedImage" runat="server" />
+        <asp:HiddenField ID="X" runat="server" />
+        <asp:HiddenField ID="Y" runat="server" />
+        <asp:HiddenField ID="W" runat="server" />
+        <asp:HiddenField ID="H" runat="server" />
+        <asp:HiddenField ID="hdnColumnWidths" runat="server" />
+    </form>
+</body>
+</html>
---
+++ OCR/OCR/Default.aspx.cs
@@ -0,0 +1,355 @@
+﻿
+using Patagames.Ocr;
+using Patagames.Ocr.Enums;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OCRExtractTable
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        #region--Page Load--
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+            }
+        }
+        #endregion
+
+        #region--btnOCRReader_Click--
+        protected void btnOCRReader_Click(object sender, EventArgs e)
+        {
+            int totalColumns = 0;
+            int.TryParse(txtColumns.Text, out totalColumns);
+
+            if (totalColumns == 0)
+            {
+                return;
+            }
+
+            string filePath = Server.MapPath("~/uploads/" + Path.GetFileName(hdnUploadedImage.Value));
+
+            // Crop Image Here & Save
+            string fileName = Path.GetFileName(filePath);
+            string cropFileName = "";
+            string cropFilePath = "";
+            if (File.Exists(filePath))
+            {
+                System.Drawing.Image orgImg = System.Drawing.Image.FromFile(filePath);
+                Rectangle CropArea = new Rectangle(
+                    Convert.ToInt32(X.Value),
+                    Convert.ToInt32(Y.Value),
+                    Convert.ToInt32(W.Value),
+                    Convert.ToInt32(H.Value));
+                try
+                {
+                    Bitmap bitMap = new Bitmap(CropArea.Width, CropArea.Height);
+                    using (Graphics g = Graphics.FromImage(bitMap))
+                    {
+                        g.DrawImage(orgImg, new Rectangle(0, 0, bitMap.Width, bitMap.Height), CropArea, GraphicsUnit.Pixel);
+                    }
+                    cropFileName = "crop_" + fileName;
+                    cropFilePath = Path.Combine(Server.MapPath("~/uploads"), cropFileName);
+                    bitMap.Save(cropFilePath);
+                    //Response.Redirect("~/UploadImages/" + cropFileName, false);
+                }
+                catch (Exception ex)
+                {
+                    //throw;
+                }
+            }
+            this.ExtractTextFromImage(cropFilePath);
+
+        }
+        #endregion
+
+        private string ExtractTextFromImage(string filePath)
+        {
+            string datapath = Server.MapPath("~");
+            List<System.Drawing.Image> cropimages = new List<System.Drawing.Image>();
+            System.Drawing.Image img = System.Drawing.Image.FromFile(filePath, true);
+
+            int totalColumns = 0;
+            int.TryParse(txtColumns.Text, out totalColumns);
+            int totalRows = 0;
+
+            using (var api = OcrApi.Create())
+            {
+                api.Init(Languages.English, datapath);
+                using (var bmp = Bitmap.FromFile(filePath) as Bitmap)
+                {
+                    api.PageSegmentationMode = PageSegMode.PSM_SINGLE_BLOCK;
+                    int temprows = api.GetTextFromImage(bmp).Replace("\n\n", "\n").Split('\n').Count();
+                    temprows = temprows > 0 ? temprows - 1 : temprows;
+                    totalRows = temprows;
+                }
+            }
+
+            cropimages = MultiCrop(filePath, img, totalRows, totalColumns);
+
+            string directorypath = Server.MapPath("~/uploads/") + Path.GetFileNameWithoutExtension(filePath);
+            if (!Directory.Exists(directorypath))
+            {
+                Directory.CreateDirectory(directorypath);
+            }
+
+            DataTable dt = new DataTable();
+            for (int i = 1; i <= totalColumns; i++)
+            {
+                dt.Columns.Add("Column" + i);
+            }
+            List<string> lstdata = new List<string>();
+
+            for (int i = 0; i < cropimages.Count; i++)
+            {
+                {
+                    string temp_crop_file = directorypath + "\\" + i + Path.GetExtension(filePath);
+                    {
+                        cropimages[i].Save(temp_crop_file);
+
+                    }
+                    try
+                    {
+                        string[] configs = { "config.cfg" };
+                        using (var api = OcrApi.Create())
+                        {
+                            api.Init(Languages.English, datapath, OcrEngineMode.OEM_DEFAULT, configs);
+                            using (var bmp = Bitmap.FromFile(temp_crop_file) as Bitmap)
+                            {
+                                api.PageSegmentationMode = PageSegMode.PSM_RAW_LINE;
+                                string extracedText =api.GetTextFromImage(bmp);
+                                if (IsValidLetter(extracedText))
+                                {
+                                    lstdata.Add(extracedText);
+                                }
+                                else
+                                {
+                                    lstdata.Add("");
+                                }
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        lstdata.Add("");
+                    }
+                }
+            }
+
+            for (int i = 0; i < lstdata.Count; i++)
+            {
+                DataRow dr = dt.NewRow();
+                for (int j = 0; j < totalColumns; j++)
+                {
+                    dr[j] = lstdata[i];
+                    i++;
+
+                    if (i >= lstdata.Count)
+                    {
+                        break;
+                    }
+                }
+                i--;
+                dt.Rows.Add(dr);
+            }
+
+            GenerateReport(dt);
+            string extractedText = "";
+            //string extractedText = modiImage.Layout.Text;
+            //modiDocument.Close();
+            return extractedText;
+        }
+
+        public List<System.Drawing.Image> MultiCrop(string filepath, System.Drawing.Image img, int row, int col)
+        {
+            List<decimal> widthlst = new List<decimal>();
+            foreach (string item in hdnColumnWidths.Value.Split(',').ToList())
+            {
+                decimal temp = 0;
+                decimal.TryParse(item, out temp);
+                widthlst.Add(temp);
+            }
+            List<System.Drawing.Image> list = new List<System.Drawing.Image>();
+            Graphics g = Graphics.FromImage(img);
+            Brush redBrush = new SolidBrush(Color.Red);
+            Pen pen = new Pen(redBrush, 3);
+            for (int i = 0; i < row; i++)
+            {
+                int xloc = 0;
+                for (int y = 0; y < col; y++)
+                {
+
+                    int tempwidth = Convert.ToInt32(Math.Floor(Convert.ToDouble((widthlst[y] * img.Width) / 100)));
+                    System.Drawing.Image temp = System.Drawing.Image.FromFile(filepath, true);
+
+
+                    //Rectangle r = new Rectangle(y * (img.Width / col),
+                    //                            i * (img.Height / row),
+                    //                            img.Width / col,
+                    //                            img.Height / row);
+                    Rectangle r = new Rectangle(xloc,
+                                                i * (img.Height / row),
+                                                tempwidth,
+                                                img.Height / row);
+                    xloc += tempwidth;
+                    g.DrawRectangle(pen, r);
+                    list.Add(cropImage(temp, r));
+                }
+            }
+            return list;
+        }
+
+        private System.Drawing.Image cropImage(System.Drawing.Image img, Rectangle cropArea)
+        {
+            Bitmap bmpImage = new Bitmap(img);
+            Bitmap bmpCrop = bmpImage.Clone(cropArea, System.Drawing.Imaging.PixelFormat.DontCare);
+            img.Dispose();
+            System.Drawing.Image orgImage = (System.Drawing.Image)(bmpCrop);
+            System.Drawing.Image newImage = resizeImage(orgImage, 400, 400);
+
+            return newImage;
+
+        }
+
+        #region--Resize Image--
+        public System.Drawing.Image resizeImage(System.Drawing.Image image, int maxWidth, int maxHeight)
+        {
+            var ratioX = (double)maxWidth / image.Width;
+            var ratioY = (double)maxHeight / image.Height;
+            var ratio = Math.Min(ratioX, ratioY);
+
+            var newWidth = (int)(image.Width * ratio);
+            var newHeight = (int)(image.Height * ratio);
+
+            var newImage = new Bitmap(newWidth, newHeight);
+            Graphics.FromImage(newImage).DrawImage(image, 0, 0, newWidth, newHeight);
+            Bitmap bmp = new Bitmap(newImage);
+
+            return (System.Drawing.Image)bmp;
+        }
+        #endregion
+
+        #region--Generate Excel--
+        private void GenerateReport(DataTable table)
+        {
+            HttpContext.Current.Response.Clear();
+            HttpContext.Current.Response.ClearContent();
+            HttpContext.Current.Response.ClearHeaders();
+            HttpContext.Current.Response.Buffer = true;
+            HttpContext.Current.Response.ContentType = "application/ms-excel";
+            HttpContext.Current.Response.Write(@"<!DOCTYPE HTML PUBLIC ""-//W3C//DTD HTML 4.0 Transitional//EN"">");
+            HttpContext.Current.Response.AddHeader("Content-Disposition", "attachment;filename=Reports.xls");
+
+            HttpContext.Current.Response.Charset = "utf-8";
+            HttpContext.Current.Response.ContentEncoding = System.Text.Encoding.GetEncoding("windows-1250");
+            //sets font
+            HttpContext.Current.Response.Write("<font style='font-size:10.0pt; font-family:Calibri;'>");
+            HttpContext.Current.Response.Write("<BR><BR><BR>");
+            //sets the table border, cell spacing, border color, font of the text, background, foreground, font height
+            HttpContext.Current.Response.Write("<Table border='1' bgColor='#ffffff' " +
+              "borderColor='#000000' cellSpacing='0' cellPadding='0' " +
+              "style='font-size:10.0pt; font-family:Calibri; background:white;'> <TR>");
+            //am getting my grid's column headers
+            //    foreach (DataColumn dc in dt.Columns)
+            //    {
+            //        Response.Write(tab + dc.ColumnName);
+            //        tab = "\t";
+            //    }
+            int columnscount = table.Columns.Count;
+
+            for (int j = 0; j < columnscount; j++)
+            {      //write in new column
+                HttpContext.Current.Response.Write("<Td>");
+                //Get column headers  and make it as bold in excel columns
+                HttpContext.Current.Response.Write("<B>");
+                HttpContext.Current.Response.Write(table.Columns[j].ToString());
+                HttpContext.Current.Response.Write("</B>");
+                HttpContext.Current.Response.Write("</Td>");
+            }
+            HttpContext.Current.Response.Write("</TR>");
+            foreach (DataRow row in table.Rows)
+            {//write in new row
+                HttpContext.Current.Response.Write("<TR>");
+                for (int i = 0; i < table.Columns.Count; i++)
+                {
+                    HttpContext.Current.Response.Write("<Td>");
+                    HttpContext.Current.Response.Write(row[i].ToString());
+                    HttpContext.Current.Response.Write("</Td>");
+                }
+
+                HttpContext.Current.Response.Write("</TR>");
+            }
+            HttpContext.Current.Response.Write("</Table>");
+            HttpContext.Current.Response.Write("</font>");
+            HttpContext.Current.Response.Flush();
+            HttpContext.Current.Response.End();
+        }
+        #endregion
+
+        #region --Is valid letter--
+        public bool IsValidLetter(string word)
+        {
+            // isolate the letters
+            string[] letters = new string[] {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"}; // other strings or letters
+                
+            bool flag = false;
+            // check if the work does not contain any letter
+            if (letters.Any(x => word.Contains(x.ToLower())))
+            {
+                flag = true;
+            }
+            return flag;
+        }
+        #endregion
+
+        protected void btnCroppedImage_Click(object sender, EventArgs e)
+        {
+            string filePath = Server.MapPath("~/uploads/" + Path.GetFileName(hdnUploadedImage.Value));
+
+            // Crop Image Here & Save
+            string fileName = Path.GetFileName(filePath);
+            string cropFileName = "";
+            string cropFilePath = "";
+            if (File.Exists(filePath))
+            {
+                System.Drawing.Image orgImg = System.Drawing.Image.FromFile(filePath);
+                Rectangle CropArea = new Rectangle(
+                    Convert.ToInt32(X.Value),
+                    Convert.ToInt32(Y.Value),
+                    Convert.ToInt32(W.Value),
+                    Convert.ToInt32(H.Value));
+                try
+                {
+                    Bitmap bitMap = new Bitmap(CropArea.Width, CropArea.Height);
+                    using (Graphics g = Graphics.FromImage(bitMap))
+                    {
+                        g.DrawImage(orgImg, new Rectangle(0, 0, bitMap.Width, bitMap.Height), CropArea, GraphicsUnit.Pixel);
+                    }
+                    cropFileName = "crop_" + fileName;
+                    cropFilePath = Path.Combine(Server.MapPath("~/uploads"), cropFileName);
+                    bitMap.Save(cropFilePath);
+                    //Response.Redirect("~/UploadImages/" + cropFileName, false);
+                }
+                catch (Exception ex)
+                {
+                    //throw;
+                }
+            }
+
+            hdnCroppedImage.Value = cropFileName;
+            divOcrProcess.Visible = true;
+            imgCropped.ImageUrl = "~/uploads/" + cropFileName;
+        }
+
+    }
+}
